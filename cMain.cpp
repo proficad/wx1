9698c405f3d2d0ceee1e44cde9e439670151327e
vjedlicka <proficad@gmail.com>
2019-12-20 16:54:28 +0100

    added menu

--- cMain.cpp
+++ cMain.cpp
@@ -13,9 +13,26 @@ cMain::cMain(wxDocManager* manager,
 	const wxSize& size) 
 	: wxDocMDIParentFrame(manager, parent, id, title, pos, size)
 {
+	wxMenu* menuFile = new wxMenu;
+
+	menuFile->Append(wxID_NEW);
+	menuFile->Append(wxID_OPEN);
+
+//	AppendDocumentFileCommands(menuFile, true);
+
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+
+
+	wxMenuBar* menubar = new wxMenuBar;
+	menubar->Append(menuFile, wxGetStockLabel(wxID_FILE));
+	SetMenuBar(menubar);
+
+	/*
 	m_pBtn	= new wxButton(this, 10001, "click me", wxPoint(10, 10), wxSize(150, 50));
 	m_txt	= new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
 	m_lst = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+	*/
 }
 
 cMain::~cMain()
